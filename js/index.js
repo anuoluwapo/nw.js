b16cd04b53432f04da41e6cb14a358fa2c85e43d
Anuoluwapo <anuoluwapokudirat@yahoo.com>
2016-07-13 17:44:46 +0000

    first comimt

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+  
+  
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+          
+
+  
